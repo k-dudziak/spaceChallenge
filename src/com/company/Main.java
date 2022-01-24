3c35956ac7489b75eaabeeecab4a960eb7cebe0e
Krzysztof Dudziak <k.dudziak@yahoo.com>
2022-01-24 19:50:32 +0100

    In theory simulation works (but returns same cost every time for each phase)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Simulation simulation = new Simulation();
+
+        File phase1file = new File("C:\\Users\\dudzkr1\\Documents\\Bootcamp Java\\SpaceChallenge\\src\\phase-1.txt");
+        File phase2file = new File("C:\\Users\\dudzkr1\\Documents\\Bootcamp Java\\SpaceChallenge\\src\\phase-2.txt");
+
+        System.out.println("Total cost for U1 (in millions of $), phase 1: " + simulation.runSimulation(simulation.loadU1(simulation.loadItems(phase1file))));
+        System.out.println("Total cost for U1 (in millions of $), phase 2: " + simulation.runSimulation(simulation.loadU1(simulation.loadItems(phase2file))));
+
+        System.out.println("Total cost for U2 (in millions of $), phase 1: " + simulation.runSimulation(simulation.loadU2(simulation.loadItems(phase1file))));
+        System.out.println("Total cost for U2 (in millions of $), phase 2: " + simulation.runSimulation(simulation.loadU2(simulation.loadItems(phase2file))));
     }
 }
---
+++ src/com/company/Simulation.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Simulation {
+
+    public ArrayList<Item> loadItems(File file) throws FileNotFoundException {
+        Scanner scannedFile = new Scanner(file);
+        ArrayList<Item> itemsList = new ArrayList();
+        Item item = new Item();
+
+        while (scannedFile.hasNextLine()){
+            String [] nameWeightArray = scannedFile.nextLine().split("=");
+            item.name = nameWeightArray[0];
+            item.weight = Integer.parseInt(nameWeightArray[1]);
+            itemsList.add(item);
+        }
+        return itemsList;
+    }
+
+    public ArrayList<Rocket> loadU1(ArrayList<Item> itemslist) {
+        ArrayList<Rocket> u1rockets = new ArrayList();
+        Rocket newU1 = new U1();
+
+        for (Item item : itemslist) {
+            if (newU1.canCarry(item)) {
+                u1rockets.add(newU1);
+            } else {
+                newU1 = new U1();
+                u1rockets.add(newU1);
+            }
+        }
+        return u1rockets;
+    }
+
+    public ArrayList<Rocket> loadU2(ArrayList<Item> itemslist) {
+        ArrayList<Rocket> u2rockets = new ArrayList();
+        Rocket newU2 = new U2();
+
+        for (Item item : itemslist) {
+            if (newU2.canCarry(item)) {
+                u2rockets.add(newU2);
+            } else {
+                newU2 = new U2();
+                u2rockets.add(newU2);
+            }
+        }
+        return u2rockets;
+    }
+
+    public int runSimulation(ArrayList<Rocket> uXrockets) {
+
+        int totalCost = 0;
+
+        for (Rocket currentRocket : uXrockets) {
+            totalCost += currentRocket.cost;
+
+            while (!currentRocket.launch() || !currentRocket.land()) {
+                totalCost += currentRocket.cost;
+            }
+        }
+        return totalCost;
+    }
+}
